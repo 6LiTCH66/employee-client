9a05ed254c52435856778af9d98bffaae9664d3a
ilja0303 <ilja200303@gmail.com>
2022-01-14 18:27:33 +0200

    validation was added

--- src/app/components/add-employee/add-employee.component.ts
+++ src/app/components/add-employee/add-employee.component.ts
@@ -3,6 +3,7 @@ import {EmployeeService} from "../../services/employee.service";
 import {Employee} from "../../models/employee";
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import {FormControl, Validators, FormGroup} from "@angular/forms";
+import{City} from "../../models/city"
 
 
 @Component({
@@ -10,19 +11,47 @@ import {FormControl, Validators, FormGroup} from "@angular/forms";
   templateUrl: './add-employee.component.html',
   styleUrls: ['./add-employee.component.css']
 })
+
 export class AddEmployeeComponent implements OnInit {
+
+  cities: City[] = [
+    {value: 'Tallinn', viewValue: 'Tallinn'},
+    {value: 'Narva', viewValue: 'Narva'},
+    {value: 'Tartu', viewValue: 'Tartu'},
+    {value: 'Viljandi', viewValue: 'Viljandi'},
+    {value: 'Rakvere', viewValue: 'Rakvere'},
+    {value: 'Maardu', viewValue: 'Maardu'},
+  ]
 
   constructor(public dialogRef: MatDialogRef<AddEmployeeComponent>,public employeeService: EmployeeService, @Inject(MAT_DIALOG_DATA) public employee: Employee) { }
 
   employeeForm: any;
+  selectedValue = new FormControl();
+  MOBILE_PATTERN = /^[0-9]{8,8}$/
 
-  formControl = new FormControl('', [
-    Validators.required
+  formControl = new FormControl("", [
+    Validators.required,
+    Validators.email
+
   ])
+  email = new FormControl('', [Validators.required, Validators.email]);
+  telephone = new FormControl('', [Validators.pattern(this.MOBILE_PATTERN), Validators.maxLength(8)])
+
+  getErrorEmailMessage() {
+    return this.email.hasError('required') ? 'Required field' :
+      this.email.hasError('email') ? 'Not a valid email' :
+        '';
+  }
+  getErrorTelephoneMessage(){
+    return this.email.hasError('required') ? 'Required field' :
+      this.email.hasError('pattern') ? 'Only numbers' :
+        '';
+  }
 
   getErrorMessage() {
-
-    return this.formControl.hasError('required') ? 'Required field' : '';
+    return this.formControl.hasError('required') ? 'Required field' :
+      this.formControl.hasError('email') ? 'Not a valid email' :
+        '';
   }
 
   ngOnInit(): void {
@@ -30,10 +59,17 @@ export class AddEmployeeComponent implements OnInit {
       firstname: new FormControl('', [Validators.required]),
       lastname: new FormControl('', [Validators.required]),
     })
+
+    this.employee.location = this.selectedValue.value
   }
   submit() {
     // emppty stuff
   }
+
+  getSelectedValue(data:any){
+    //this.employee.location = data.value;
+  }
+
   onNoClick():void{
     this.dialogRef.close();
   }
--- src/app/components/edit-employee/edit-employee.component.ts
+++ src/app/components/edit-employee/edit-employee.component.ts
@@ -2,6 +2,7 @@ import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import {EmployeeService} from "../../services/employee.service";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {City} from "../../models/city";
 
 @Component({
   selector: 'app-edit-employee',
@@ -13,18 +14,41 @@ export class EditEmployeeComponent implements OnInit {
   constructor(public dialogRef: MatDialogRef<EditEmployeeComponent>, @Inject(MAT_DIALOG_DATA) public employee: any, public employeeService:EmployeeService) { }
 
   employeeForm: any;
+  selectedValue = new FormControl();
+  MOBILE_PATTERN = /^[0-9]{8,8}$/
+
+  cities: City[] = [
+    {value: 'Tallinn', viewValue: 'Tallinn'},
+    {value: 'Narva', viewValue: 'Narva'},
+    {value: 'Tartu', viewValue: 'Tartu'},
+    {value: 'Viljandi', viewValue: 'Viljandi'},
+    {value: 'Rakvere', viewValue: 'Rakvere'},
+    {value: 'Maardu', viewValue: 'Maardu'},
+  ]
 
   formControl = new FormControl('', [
     Validators.required
   ])
+  email = new FormControl('', [Validators.required, Validators.email]);
+  telephone = new FormControl('', [Validators.pattern(this.MOBILE_PATTERN), Validators.maxLength(8)])
 
   getErrorMessage() {
 
     return this.formControl.hasError('required') ? 'Required field' : '';
   }
+  getErrorEmailMessage() {
+    return this.email.hasError('required') ? 'Required field' :
+      this.email.hasError('email') ? 'Not a valid email' :
+        '';
+  }
+  getErrorTelephoneMessage(){
+    return this.email.hasError('required') ? 'Required field' :
+      this.email.hasError('pattern') ? 'Only numbers' :
+        '';
+  }
 
   submit() {
-    // emppty stuff
+
   }
 
   ngOnInit(): void {
