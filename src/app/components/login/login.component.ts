116bae55c6e841c1d896d0da814b3afb2db3c28b
ilja0303 <ilja200303@gmail.com>
2022-01-28 12:25:09 +0200

    login fix

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -8,8 +8,7 @@ import {HttpClient} from "@angular/common/http";
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit, OnDestroy {
-  private readonly TOKEN_URL = 'https://employee-webserver.herokuapp.com/auth/token'
+export class LoginComponent implements OnInit{
 
   constructor( private authService: AuthService, private http: HttpClient) {
 
@@ -25,20 +24,8 @@ export class LoginComponent implements OnInit, OnDestroy {
 
   }
 
-  newToken:any;
-
-
   ngOnInit(): void {
-    if(localStorage.getItem("currentUser")){
-      this.newToken = setInterval(() => {
-        this.http.post(this.TOKEN_URL, "", {withCredentials: true}).subscribe(()=>{})
-      }, 14 * 60 * 1000)
-
-    }
   }
 
-  ngOnDestroy(): void {
-    clearInterval(this.newToken);
-  }
 
 }
